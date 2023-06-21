acbd26cff2d85467ee89cf78cc0e1e109853f8ec
Slavyasanek <117304306+Slavyasanek@users.noreply.github.com>
2023-06-21 18:21:19 +0300

    add local storage to redux

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,11 +4,6 @@ import { Filter } from "./Filter/Filter";
 import { ContactList } from "./ContactList/ContactList";
 
 export const App = () => {
-
-  // const filterContacts = () => {
-  //   return contacts.filter(contact => contact.name.toLowerCase().includes(filter.toLowerCase()))
-  // }
-
   return (
     <div>
       <Section
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,27 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { configureStore, combineReducers } from "@reduxjs/toolkit";
 import { contactsReducer } from "./contactsSlice";
 import { filterReducer } from "./filterSlice";
+import storage from 'redux-persist/lib/storage';
+import { persistStore, persistReducer } from 'redux-persist';
+
+const persistConfig = {
+    key: 'contacts',
+    storage
+}
+
+const reducers = combineReducers({
+    contacts: contactsReducer,
+    filter: filterReducer
+})
+
+const persistedReducer = persistReducer(persistConfig, reducers)
 
 export const store = configureStore({
-    reducer: {
-        contacts: contactsReducer,
-        filter: filterReducer,
-    }
+    reducer: persistedReducer,
+    middleware: getDefaultMiddleware =>
+        getDefaultMiddleware({
+            serializableCheck: false,
+        }),
 });
+
+export const persistor = persistStore(store);
